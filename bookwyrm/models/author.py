5998525efe67dded60aabc67fd316be00646ac69
Mouse Reeve <mousereeve@riseup.net>
2024-03-19 15:10:30 -0700

    Merge pull request #3244 from bookwyrm-social/dependabot/pip/pillow-10.2.0
    Bump pillow from 10.0.1 to 10.2.0

---
+++ bookwyrm/models/author.py
@@ -0,0 +1,75 @@
+""" database schema for info about authors """
+import re
+from typing import Tuple, Any
+
+from django.contrib.postgres.indexes import GinIndex
+from django.db import models
+
+from bookwyrm import activitypub
+from bookwyrm.settings import DOMAIN
+
+from .book import BookDataModel
+from . import fields
+
+
+class Author(BookDataModel):
+    """basic biographic info"""
+
+    wikipedia_link = fields.CharField(
+        max_length=255, blank=True, null=True, deduplication_field=True
+    )
+    isni = fields.CharField(
+        max_length=255, blank=True, null=True, deduplication_field=True
+    )
+    gutenberg_id = fields.CharField(
+        max_length=255, blank=True, null=True, deduplication_field=True
+    )
+    isfdb = fields.CharField(
+        max_length=255, blank=True, null=True, deduplication_field=True
+    )
+
+    website = fields.CharField(
+        max_length=255, blank=True, null=True, deduplication_field=True
+    )
+    # idk probably other keys would be useful here?
+    born = fields.DateTimeField(blank=True, null=True)
+    died = fields.DateTimeField(blank=True, null=True)
+    name = fields.CharField(max_length=255)
+    aliases = fields.ArrayField(
+        models.CharField(max_length=255), blank=True, default=list
+    )
+    bio = fields.HtmlField(null=True, blank=True)
+
+    def save(self, *args: Tuple[Any, ...], **kwargs: dict[str, Any]) -> None:
+        """normalize isni format"""
+        if self.isni:
+            self.isni = re.sub(r"\s", "", self.isni)
+
+        return super().save(*args, **kwargs)
+
+    @property
+    def isni_link(self):
+        """generate the url from the isni id"""
+        clean_isni = re.sub(r"\s", "", self.isni)
+        return f"https://isni.org/isni/{clean_isni}"
+
+    @property
+    def openlibrary_link(self):
+        """generate the url from the openlibrary id"""
+        return f"https://openlibrary.org/authors/{self.openlibrary_key}"
+
+    @property
+    def isfdb_link(self):
+        """generate the url from the isni id"""
+        return f"https://www.isfdb.org/cgi-bin/ea.cgi?{self.isfdb}"
+
+    def get_remote_id(self):
+        """editions and works both use "book" instead of model_name"""
+        return f"https://{DOMAIN}/author/{self.id}"
+
+    activity_serializer = activitypub.Author
+
+    class Meta:
+        """sets up postgres GIN index field"""
+
+        indexes = (GinIndex(fields=["search_vector"]),)
---
+++ bookwyrm/tests/test_book_search.py
@@ -0,0 +1,142 @@
+""" test searching for books """
+import datetime
+from django.test import TestCase
+from django.utils import timezone
+
+from bookwyrm import book_search, models
+from bookwyrm.connectors.abstract_connector import AbstractMinimalConnector
+
+
+class BookSearch(TestCase):
+    """look for some books"""
+
+    @classmethod
+    def setUpTestData(self):  # pylint: disable=bad-classmethod-argument
+        """we need basic test data and mocks"""
+        self.work = models.Work.objects.create(title="Example Work")
+
+        self.first_edition = models.Edition.objects.create(
+            title="Example Edition",
+            parent_work=self.work,
+            isbn_10="0000000000",
+            physical_format="Paperback",
+            published_date=datetime.datetime(2019, 4, 9, 0, 0, tzinfo=timezone.utc),
+        )
+        self.second_edition = models.Edition.objects.create(
+            title="Another Edition",
+            parent_work=self.work,
+            isbn_10="1111111111",
+            openlibrary_key="hello",
+            pages=150,
+        )
+        self.third_edition = models.Edition.objects.create(
+            title="Another Edition with annoying ISBN",
+            parent_work=self.work,
+            isbn_10="022222222X",
+        )
+
+    def test_search(self):
+        """search for a book in the db"""
+        # title/author
+        results = book_search.search("Example")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0], self.first_edition)
+
+        # isbn
+        results = book_search.search("0000000000")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0], self.first_edition)
+
+        # identifier
+        results = book_search.search("hello")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0], self.second_edition)
+
+    def test_isbn_search(self):
+        """test isbn search"""
+        results = book_search.isbn_search("0000000000")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0], self.first_edition)
+
+    def test_search_identifiers(self):
+        """search by unique identifiers"""
+        results = book_search.search_identifiers("hello")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0], self.second_edition)
+
+    def test_search_identifiers_isbn_search(self):
+        """search by unique ID with slightly wonky ISBN"""
+        results = book_search.search_identifiers("22222222x")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0], self.third_edition)
+
+    def test_search_identifiers_return_first(self):
+        """search by unique identifiers"""
+        result = book_search.search_identifiers("hello", return_first=True)
+        self.assertEqual(result, self.second_edition)
+
+    def test_search_title_author(self):
+        """search by unique identifiers"""
+        results = book_search.search_title_author("annoying", min_confidence=0)
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0], self.third_edition)
+
+    def test_search_title_author_return_first(self):
+        """sorts by edition rank"""
+        result = book_search.search_title_author(
+            "Another", min_confidence=0, return_first=True
+        )
+        self.assertEqual(result, self.second_edition)  # highest edition rank
+
+    def test_search_title_author_one_edition_per_work(self):
+        """at most one edition per work"""
+        results = book_search.search_title_author("Edition", 0)
+        self.assertEqual(results, [self.first_edition])  # highest edition rank
+
+    def test_format_search_result(self):
+        """format a search result"""
+        result = book_search.format_search_result(self.first_edition)
+        self.assertEqual(result["title"], "Example Edition")
+        self.assertEqual(result["key"], self.first_edition.remote_id)
+        self.assertEqual(result["year"], 2019)
+
+        result = book_search.format_search_result(self.second_edition)
+        self.assertEqual(result["title"], "Another Edition")
+        self.assertEqual(result["key"], self.second_edition.remote_id)
+        self.assertIsNone(result["year"])
+
+    def test_search_result(self):
+        """a class that stores info about a search result"""
+        models.Connector.objects.create(
+            identifier="example.com",
+            connector_file="openlibrary",
+            base_url="https://example.com",
+            books_url="https://example.com/books",
+            covers_url="https://example.com/covers",
+            search_url="https://example.com/search?q=",
+            isbn_search_url="https://example.com/isbn?q=",
+        )
+
+        class TestConnector(AbstractMinimalConnector):
+            """nothing added here"""
+
+            def get_or_create_book(self, remote_id):
+                pass
+
+            def parse_search_data(self, data, min_confidence):
+                return data
+
+            def parse_isbn_search_data(self, data):
+                return data
+
+        test_connector = TestConnector("example.com")
+        result = book_search.SearchResult(
+            title="Title",
+            key="https://example.com/book/1",
+            author="Author Name",
+            year="1850",
+            connector=test_connector,
+        )
+        # there's really not much to test here, it's just a dataclass
+        self.assertEqual(result.confidence, 1)
+        self.assertEqual(result.title, "Title")
